bae69dabfbe482615ea7ab95fe3f6197536ce322
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-02-08 08:20:19 -0600

    Homework 2 solution

--- homework/02_decisions/decisions.cpp
+++ homework/02_decisions/decisions.cpp
@@ -1,3 +1,4 @@
+#include "decisions.h"
 //write include statement for decisions header
 
 //Write code for function get_grade_points that accepts a string letter_grade and returns 
@@ -8,14 +9,40 @@
 //given grade "D" returns 1
 //given grade "F" returns 0
 //another other value return -1
-
-
-
-
+int get_grade_points(std::string letter_grade)
+{
+	if (letter_grade == "A") 
+	{
+		return 4;
+	}
+	else if(letter_grade == "B") 
+	{
+		return 3;
+	}
+	else if (letter_grade == "C")
+	{
+		return 2;
+	}
+	else if (letter_grade == "D")
+	{
+		return 1;
+	}
+	else if (letter_grade == "F")
+	{
+		return 0;
+	}
+	else 
+	{
+		return -1;
+	}
+}
 
 
 //Write code for function calculate_gpa that accepts an int named credit_hours and
 //a double named credit_points.  The function returns the quotient of credit_points divided by
 //credit_hours. In the function account for division by zero by returning a -1.
-
+double calculate_gpa(int credit_hours, double credit_points)
+{
+	return credit_points / credit_hours;
+}
 
--- homework/02_decisions/main.cpp
+++ homework/02_decisions/main.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
+#include<string>
+using std::cout; using std::cin; using std::string;
 //write include statements
+#include "decisions.h"
 
 /*
 In main write code to capture a letter_grade and credit_hours from the keyboard:
@@ -23,7 +26,33 @@ GPA 3.0
 */
 int main() 
 {
-	std::cout << "Hello" ;
+	string letter_grade;
+	int credit_hours, sum_credit_hours = 0, sum_credit_points = 0;
+
+	cout << "Enter letter grade: ";
+	cin >> letter_grade;
+	cout << "Enter credit hours: ";
+	cin >> credit_hours;
+	sum_credit_points += get_grade_points(letter_grade) * credit_hours;
+	sum_credit_hours += credit_hours;
+
+	cout << "Enter letter grade: ";
+	cin >> letter_grade;
+	cout << "Enter credit hours: ";
+	cin >> credit_hours;
+	sum_credit_points += get_grade_points(letter_grade)  * credit_hours;
+	sum_credit_hours += credit_hours;
+
+	cout << "Enter letter grade: ";
+	cin >> letter_grade;
+	cout << "Enter credit hours: ";
+	cin >> credit_hours;
+	sum_credit_points += get_grade_points(letter_grade)  * credit_hours;
+	sum_credit_hours += credit_hours;
+
+
+	double gpa = calculate_gpa(sum_credit_hours, sum_credit_points);
+	cout << "GPA: " << gpa;
 
 	return 0;
 }
