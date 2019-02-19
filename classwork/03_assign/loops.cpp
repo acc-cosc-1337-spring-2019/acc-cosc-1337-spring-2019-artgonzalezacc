19a0d7dd9791ba5c2e508429931ffe9b9e33c5bf
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-02-18 22:47:32 -0600

    Classwork 3 solution

--- classwork/03_assign/loops.cpp
+++ classwork/03_assign/loops.cpp
@@ -10,3 +10,14 @@ returns 120
 
 DON'T FORGET TO WRITE TEST CASE.  See file loop_test.cpp
 */
+int factorial(int num) 
+{
+	int f = 1;
+
+	for (int i = 1; i <= num; ++i) 
+	{
+		f = f * i;
+	}
+
+	return f;
+}
