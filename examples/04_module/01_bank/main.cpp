2b284a19815cf3b1f430ae590c42233a5f8ae7b6
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-02-21 21:50:31 -0600

    Module 4 Bank example

--- examples/04_module/01_bank/main.cpp
+++ examples/04_module/01_bank/main.cpp
@@ -1,9 +1,20 @@
 #include "bank_account.h"
+#include<vector>
+#include<iostream>
 
-int main() 
+int main()
 {
-	BankAccount account(12345689, 500);
-	BankAccount account1(55545689, 5000);
+	std::vector<BankAccount>accounts;
+	BankAccount account(12345689, 500);  //object1
+	BankAccount account1(55545689, 5000); //object2
+
+	accounts.push_back(account);
+	accounts.push_back(account1);
+
+	for (auto act : accounts)
+	{
+		std::cout << act.get_balance() << "\n";
+	}
 
 	return 0;
 }
