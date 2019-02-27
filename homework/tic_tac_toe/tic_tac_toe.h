639f95ba863d9a462a1251afb817f0e2b647e873
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-02-26 22:09:10 -0600

    Tic tac toe homework tips

--- homework/tic_tac_toe/tic_tac_toe.h
+++ homework/tic_tac_toe/tic_tac_toe.h
@@ -1,10 +1,18 @@
 #include<string>
+#include<vector>
 
 class TicTacToe 
 {
 public:
 	void start_game(std::string first_player);
 	std::string get_player() const;
+	bool game_over();
+	void mark_board(int position);
+
 private:
 	std::string next_player;
+	std::vector<std::string> pegs{ 9, " " };
+
+	void set_next_player();
+	bool check_column_win();
 };
