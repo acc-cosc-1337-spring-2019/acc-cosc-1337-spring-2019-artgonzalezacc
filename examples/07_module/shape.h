d88512fc3d45967b5af10edd8be0030605a75b42
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-03-29 16:20:46 -0500

    07 module shapes

---
+++ examples/07_module/shape.h
@@ -0,0 +1,12 @@
+
+#ifndef SHAPE_H
+#define SHAPE_H
+//h
+class Shape 
+{
+public:
+	//virtual void draw();this is a virtual function
+	virtual void draw() =0;//pure virtual function
+};
+
+#endif //Shape_H
