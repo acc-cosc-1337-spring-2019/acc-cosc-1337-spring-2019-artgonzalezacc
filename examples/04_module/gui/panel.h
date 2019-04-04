9344e93ee336d5f16819b5ba1501dd5016e63790
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-04-04 18:38:29 -0500

    Bank account example

---
+++ examples/04_module/gui/panel.h
@@ -0,0 +1,26 @@
+#ifndef PANEL_H
+#define PANEL_H
+#include<wx/wx.h>
+#include "checking_account.h"
+
+class Panel : public wxPanel
+{
+public:
+	Panel(wxWindow* parent);
+	
+private:
+	void on_submit_button_click(wxCommandEvent & event);
+	/*
+	Organize components in Sizer
+	*/
+	wxBoxSizer* get_box_sizer();
+	wxStaticText* balance_label;
+	wxTextCtrl* transaction_text;
+	wxButton* submit_button;
+	wxStaticText* balance_text;
+	wxRadioBox* trans_type_radio;
+	std::unique_ptr<BankAccount> account;
+
+};
+
+#endif // !PANEL_H
