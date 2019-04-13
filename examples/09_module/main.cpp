cb4f6f69c31c4a2cd8a1bdbc55187e2fb9805bf2
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-04-13 10:08:22 -0500

    Module 9 File output input example

---
+++ examples/09_module/main.cpp
@@ -0,0 +1,23 @@
+#include<fstream>
+#include<iostream>
+#include <string>
+#include <vector>
+#include "temperature.h"
+#include "temperature_data.h"
+
+int main() 
+{
+	std::vector<Temperature> temps{ Temperature(3, 79.5), Temperature(4, 85.9) };
+
+	TemperatureData data("temps.txt");
+	data.save_readings(temps);
+
+	auto readings = data.get_readings();
+
+	for (auto r : readings) 
+	{
+		std::cout << r.get_hour() << " " << r.get_reading() << "\n";
+	}
+
+	return 0;
+}
