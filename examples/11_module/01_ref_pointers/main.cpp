ff5a0a9e801d88a49a506ff5cb3ff87fb5f31d44
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-04-25 17:39:20 -0500

    Reference and pointers example

--- examples/11_module/01_ref_pointers/main.cpp
+++ examples/11_module/01_ref_pointers/main.cpp
@@ -1,5 +1,20 @@
+#include "ref_pointers.h"
+#include<iostream>
+
 int main() 
 {
-	
+	int number = 10;
+	//a pointer can store the address of a variable
+	int* numberPtr = &number;
+
+	ref(number, numberPtr);
+
+	//display what value? we should see 100
+
+	std::cout << "Address of number: " << &number << " value is: "<<number<<"\n";
+
+
+	std::cout << "\nAddress of numberPtr: " << &numberPtr << " value stored at address pointed to is: " << numberPtr<<"\n Dereference numberPtr: "<<*numberPtr;
+
 	return 0;
 }
